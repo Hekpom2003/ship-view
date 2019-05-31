6297acdb3965c64b3544a0481078fdd612794ee0
dmitry <d.titov@tireos.pro>
2019-05-31 10:38:04 +0300

    first commit

---
+++ src/Components/ShipRoom/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class ShipRoom extends React.Component{
+  render() {
+
+    console.log('ShipRoom',this.props);
+
+    return (
+      <div className="ship-room">
+
+      </div>
+    );
+  }
+}
+
+export default ShipRoom;
